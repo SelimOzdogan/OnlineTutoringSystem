8118ef55fb787316f8e69643fb04601bb4bc9c60
@rodrickmascarenhas <30309234+rodrickmascarenhas@users.noreply.github.com>
2021-11-28 13:03:40 -0800

    Add files via upload
    Adding Recycler

--- app/src/main/java/com/example/onlinetutoringsystem/MainActivity.java
+++ app/src/main/java/com/example/onlinetutoringsystem/MainActivity.java
@@ -1,69 +1,38 @@
-package com.example.onlinetutoringsystem;
+package com.example.otslistview;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.onlinetutoringsystem.Data.UserDao;
-import com.example.onlinetutoringsystem.Data.UserDatabase;
-import com.example.onlinetutoringsystem.Model.User;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editTextEmail, editTextPassword;
-    Button buttonLogin;
-    TextView textViewRegister;
-    UserDao db;
-    UserDatabase dataBase;
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+    Spinner courseDropDown, price_DropDown, AvailableDropDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
 
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        buttonLogin = findViewById(R.id.buttonLogin);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#000000"));
+        actionBar.setBackgroundDrawable(colorDrawable);
 
-        textViewRegister = findViewById(R.id.textViewRegister);
-
-        dataBase = Room.databaseBuilder(this, UserDatabase.class, "mi-database.db")
-                .allowMainThreadQueries()
-                .build();
-
-        db = dataBase.getUserDao();
-
-
-        textViewRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
-            }
-        });
-
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = editTextEmail.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
-
-                User user = db.getUser(email, password);
-                if (user != null) {
-                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
-                    i.putExtra("User", user);
-                    startActivity(i);
-                    finish();
-                }else{
-                    Toast.makeText(MainActivity.this,
-                            "Unregistered user, or incorrect", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new UserRecycler();
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
     }
 }
